51af6e34583390bf1bf26ccf423ad9aedcea7aa2
Benish <bsarinelli@gmail.com>
2018-11-04 23:14:21 -0800

    Adding config file with updates and syntax changes to html and js files.

--- simple.js
+++ simple.js
@@ -1,4 +1,9 @@
-var quotes = ['A', 'B', 'C', 'D']
+
+var quotes = ['The Journey is never ending...-unknown', 
+'Live as if you were to die tomorrow. Learn as if you were to live forever.-Mahatma Gandhi', 
+'Be brave enough to live life creatively.  The creative place where no one else has ever been.-Alan Alda', 
+'Choosing to be positive and having a grateful attitude is going to determine how you are going to live your life.-Joel Osteen'
+'I have not failed, I have just found 10k ways that will not work.-Thomas Alba Edison']
 
 function newQuote() {
 	var randomNumber = Math.floor(Math.random() * (quotes.length));
